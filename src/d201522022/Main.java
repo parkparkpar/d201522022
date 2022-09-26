630900a25767a95727b732633425278ac4dacb17
Park, Jeonghwan <gpark0309@gmail.com>
2022-09-26 15:32:21 +0900

    Main.java 수정 #3

--- src/d201522022/Main.java
+++ src/d201522022/Main.java
@@ -3,8 +3,9 @@ package d201522022;
 public class Main {
     
     public static void main(String[] args) {
-        String s = "hello world";
+        String s = "안녕하세요";
         System.out.println(s);
     }
 }
+
 
